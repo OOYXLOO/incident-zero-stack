11df05e09ce1ee8fd585b0234d77ed8d339d8b55
yxl <2350312617@qq.com>
2026-06-24 09:00:03 +0800

    Keep gh-pages static demo only

--- api/cloud-readiness.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-
-const { handleVercelRequest } = require("./_handler");
-
-module.exports = (request, response) => handleVercelRequest("cloud-readiness", request, response);
--- api/storage-preview.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-
-const { handleVercelRequest } = require("./_handler");
-
-module.exports = (request, response) => handleVercelRequest("storage-preview", request, response);
